df79732d01f32e5c33e2fcb0e17f936a20a4c2b6
Josh Teperman <joshteperman@gmail.com>
2019-05-27 19:48:04 +1000

    added 'bookmark' button and event WIP

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ module.exports = function (n) { return n * 111 }
 // To bundle app.js file, run browserify app.js > bundle.js
 
 const COLORS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-
+let gradientMemory = []
 
 
   // events -->
 const eventListeners = () => {
-
-
-  const clickSubmitEvent = () => {
-  }
-  const clickCopyGradientEvent = () => {
-  }
-  
+  console.log('adding event listeners')
   const clickRandomGradientEvent = () => {
     const button = document.querySelector('#random-gradient-button').addEventListener('click', () => {
       setNewGradientBackground(randomHexoColor(), randomHexoColor());
@@ -27,22 +21,42 @@ const eventListeners = () => {
     clickTarget.addEventListener('click', copyText())
   }
 
+  const clickPreviousGradientEvent = () => {
+    const button = document.querySelector('#previous-gradient-button')
+    button.addEventListener('click', () => {
+      // show previous gradient
+    })
+  }
+
+  const clickBookmarkedGradientEvent = () => {
+    const button = document.querySelector('#bookmark-gradient-button');
+    button.addEventListener('click', (event) => {
+      console.log('clicked bookmark')
+      let storageData = JSON.parse(localStorage.gradientMemory)
+      const newBookmark = document.createElement('li')
+      newBookmark.className = "bookmark"
+      newBookmark.style.background = `${storageData[storageData.length - 1]}`
+      console.log(newBookmark)
+      document.querySelector('#bookmarks').appendChild(newBookmark)  
+      // debugger
+    })
+  }
+
   clickRandomGradientEvent();
   copyGradientEvent();
+  clickBookmarkedGradientEvent();
+
 }
 
-
-
-  
 // logic -->
 
 const setNewGradientBackground = (color1, color2) => {
   const newGradient = createGradientString(color1, color2) 
   document.querySelector('body').style = `background: ${newGradient}`
-
+  updateGradientMemory(newGradient);
   updateColorPicker(document.querySelector('#color1'), color1);
   updateColorPicker(document.querySelector('#color2'), color2);
-  udpateResult(newGradient);
+  updateResult(newGradient);
 }
 
 const randNum = () => {
@@ -61,28 +75,40 @@ const randomHexoColor = () => {
   return '#' + color.join("")
 }
 
+const updateGradientMemory = (gradientString) => {
+  if (localStorage.gradientMemory) {
+    // console.log('gradientMemory found in localStorage')
+    gradientMemory = JSON.parse(localStorage.gradientMemory)
+    // console.log('fetched gradient memory from localStorage')
+  }
+
+  if (!gradientMemory.includes(gradientString)) {
+    // console.log(gradientMemory)
+    gradientMemory.push(gradientString);
+    localStorage.setItem('gradientMemory', JSON.stringify(gradientMemory));
+    // console.log(`updated gradient memory: ${gradientString}`)
+    // console.log(localStorage.gradientMemory)
+  }
+}
+
 const updateColorPicker = (colorPicker, color) => {
   colorPicker.value = color
 }
 
-const udpateResult = (gradient) => {
+const updateResult = (gradient) => {
   document.querySelector('#gradient-string').value = `background: ${gradient}`
 }
 
 const copyText = () => {
-  console.log('hello from inside copy text')
   const copyText = document.querySelector('#gradient-string');
   // copyText.select();
   // document.execCommand('copy');
   // console.log(`${copytText}`)
 }
 
-    
 const run = () => {
   setNewGradientBackground(randomHexoColor(), randomHexoColor());
 }
-
-
 
 run();
 eventListeners();
